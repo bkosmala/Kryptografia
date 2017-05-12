33a0fa8af6dd308e8d2acec0e5a6c2a4af2fe1c2
Piotr Wójcik <piotr.w.1995@gmail.com>
2017-05-13 01:22:11 +0200

    Porównywanie kluczy

--- Zad 3 - RSA/src/crypto/zad3/RSAKey.java
+++ Zad 3 - RSA/src/crypto/zad3/RSAKey.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class RSAKey {
 
@@ -33,5 +34,22 @@ public class RSAKey {
     
     public List<BigInteger> getValuesList() {
         return valueList;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof RSAKey) {
+            RSAKey other = (RSAKey)obj;
+            return this.valueList.equals(other.getValuesList());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.valueList);
+        return hash;
     }
 }
